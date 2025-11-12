cb987b706bd14d46d8048e8396908b83799544db
Aiden Lehrhaupt <aidenlehrhaupt@Aidens-MacBook-Pro.local>
2025-11-12 12:44:16 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -20,6 +20,8 @@ def div(a, b):
         raise ZeroDivisionError
     return b / a
 def log(a, b):
+    if a<=0 or a == 1:
+        raise ValueError
     if b <= 0:
         raise ValueError
     return math.log(b, a)
